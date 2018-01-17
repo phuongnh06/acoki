20d36ebb7f7e3508857e1ef506ba21074204ac12
phuongnh <phuongnh0606@gmail.com>
2018-01-17 16:43:35 +0700

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -221,14 +221,14 @@
                 <div style="text-align: center">
                     <p style="width: 140px; border-top: 2px solid #9bbeda; display: inline-block"></p>
                 </div>
+                <span class="icorn icorn-open icorn-top-left"></span>
+                <span class="icorn icorn-close icon-bottom-right"></span>
                 <div class="customer-detail detail-1">
                     <div class="customer-info">
                         <img src="https://scontent.fhan3-1.fna.fbcdn.net/v/t1.0-9/10590573_498302647016806_8415941919985221539_n.jpg?oh=5451b9cee3b49371bfc0f46cd81e57e3&oe=5ABCAFBA">
                         <p class="h3">Đào Quang Duy - Leader Team Tester</p>
                     </div>
                     <div class="customer-comment">
-                        <span class="icorn icorn-open icorn-top-left"></span>
-                        <span class="icorn icorn-close icon-bottom-right"></span>
                         <p>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque lobortis,
                             velit eu interdum rhoncus, massa neque commodo risus, ac eleifend mauris augue vitae lectus.
@@ -244,8 +244,6 @@
                         <p class="h3">Nguyễn Minh Đức - Dev Acoki</p>
                     </div>
                     <div class="customer-comment">
-                        <span class="icorn icorn-open icorn-top-left"></span>
-                        <span class="icorn icorn-close icon-bottom-right"></span>
                         <p>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                         </p>
@@ -257,8 +255,6 @@
                         <p class="h3">Bùi Quang Phú - Leader Team Marketing</p>
                     </div>
                     <div class="customer-comment">
-                        <span class="icorn icorn-open icorn-top-left"></span>
-                        <span class="icorn icorn-close icon-bottom-right"></span>
                         <p>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque lobortis,
                             velit eu interdum rhoncus, massa neque commodo risus, ac eleifend mauris augue vitae lectus.
